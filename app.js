47f4190295e07ace4b7f5682e8977250dfadfe38
Adonis K <aklp08@gmail.com>
2014-01-07 03:14:01 +0200

    working both aliased and un-aliased options

--- app.js
+++ app.js
@@ -18,21 +18,36 @@ if (args.length === 2) {
 args.shift(0);
 args.shift(0);
 
+var unknownOption = function(option) {
+	if (option) {
+		console.log('unknown option "' + option + '"');
+	}
+}
+
 // create a new variable which contains functions from the vejstools library
 var options = require('vejstools-lib');
 
 // loop through all the CLI arguments and process them
 for (var i = 0; i < args.length; i++) {
 	if (args[i][0] === '-') {
-			// if current item is an option
-		if (args[i][1] === '-') {
-			// if current item is not an aliased option
-			// TODO: "--encoding" type of options
-		} else {
+		// if current item is an option
+		// create new variables which will store the option target and the result which is locked
+		var option;
+		var result = false;
+		if (args[i].length === 2) {
 			// then the current item is an aliased option
 			// store the option's first letter in the option variable
-			var option = args[i][1];
-			if (args[i+1][0] === '-'){
+			option = args[i][1];
+		} else {
+			// TODO: make un-aliased options work
+			// if current item is not an aliased option
+			// store the option's full name in the option variable
+			option = args[i].slice(2);
+		}
+
+		if (options[option]) {
+			// if this option exists in the vejstools-lib library
+			if (args[i + 1][0] === '-') {
 				// if the next item is an option
 				// execute the current option's command with no parameters and show results
 				console.log(options[option]());
@@ -40,10 +55,21 @@ for (var i = 0; i < args.length; i++) {
 				// then the current item has parameters
 				// store the current item's parameters in the parameter variable
 				// execute current option's command with the parameter specified and show results
-				var parameter = args[i+1];
+				var parameter = args[i + 1];
 				// show results
-				console.log(options[option](parameter));
+				result = options[option](parameter);
 			}
+		} else {
+			// unknown option
+			unknownOption(option);
+		}
+
+		if (result) {
+			// if a result exists
+			// print result on screen
+			console.log(result);
+			// reset result lock
+			result = false;
 		}
 	}
 }
